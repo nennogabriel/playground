9cb970f6e3758d28a895df1cd422374ea1625d16
Pedro Nascimento <nennogabriel@gmail.com>
2019-10-17 17:33:31 -0300

    first integration test

--- gae/playgroundjs/backend/__tests__/integration/controllers/user.test.js
+++ gae/playgroundjs/backend/__tests__/integration/controllers/user.test.js
@@ -12,4 +12,23 @@ describe('User', () => {
       })
     expect(response.body).toHaveProperty('id')
   })
+
+  it('should not be able to register with duplicated email', () => {
+    await request(app)
+      .post('/api/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'diego@fernandes.com.br',
+        password: '123456'
+      })
+
+    const response = await request(app)
+      .post('/api/users')
+      .send({
+        name: 'Diego Fernandes',
+        email: 'diego@fernandes.com.br',
+        password: '123456'
+      })
+    expect(response.status).toBe(400)
+  })
 })
