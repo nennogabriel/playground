1f2a0982203125056ae236d4905b40a465305df0
Pedro Nascimento <nennogabriel@gmail.com>
2019-11-02 20:11:43 -0300

    gatsby: load md files to index page

--- gatsby-bootcamp/gatsby-config.js
+++ gatsby-bootcamp/gatsby-config.js
@@ -9,5 +9,15 @@ module.exports = {
     title: "Full-Stack Bootcamp",
     author: "Pedro Moreno",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    "gatsby-transformer-remark",
+  ],
 }
--- gatsby-bootcamp/src/pages/blog.js
+++ gatsby-bootcamp/src/pages/blog.js
@@ -1,13 +1,36 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql, useStaticQuery } from "gatsby"
 
 // import { Container } from './styles';
 
 export default function BlogPage() {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here later on.</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map(post => (
+          <li key={post.node.frontmatter.title}>
+            <h2>{post.node.frontmatter.title}</h2>
+            <p>{post.node.frontmatter.date}</p>
+          </li>
+        ))}
+      </ol>
     </Layout>
   )
 }
