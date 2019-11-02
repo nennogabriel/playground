ec90d60e06ca97242d75375897633ab74e374424
Pedro Nascimento <nennogabriel@gmail.com>
2019-11-02 19:00:35 -0300

    gatsby: basics done

--- gatsby-bootcamp/src/components/layout.js
+++ gatsby-bootcamp/src/components/layout.js
@@ -3,13 +3,16 @@ import Header from "./header"
 import Footer from "./footer"
 
 import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 // import { Container } from './styles';
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <Header />
-      {children}
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
+        {children}
+      </div>
       <Footer />
     </div>
   )
